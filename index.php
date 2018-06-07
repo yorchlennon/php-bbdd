4af7e046163af651bd9398fa9d6ad5172e4cb7e4
Jorge Ruiz <jruiz1987@gmail.com>
2018-06-07 10:38:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
-/*$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-} else {
-    printf("Connected to the database");
-}
-$connection->close();*/
-$prueba = getenv("databaseuser")
-echo $prueba;
 echo "segunda linea";
 ?>
