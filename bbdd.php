eb35cb91042b9e10afeabf050420c961e2f9d767
Jorge Ruiz <jruiz1987@gmail.com>
2018-06-07 11:29:14 +0200

    Update bbdd.php

--- bbdd.php
+++ bbdd.php
@@ -11,5 +11,18 @@ if ($connection->connect_errno) {
 } else {
     printf("Connected to the database");
 }
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($connection->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $connection->error;
+}
 $connection->close();
 ?>
